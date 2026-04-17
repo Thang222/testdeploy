6836e50235d42a48b485f76772e954fac0b6e2ed
Thang222 <thangnx.it@gmail.com>
2026-04-17 09:29:43 +0700

    log

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ import express from "express";
 import mysql from "mysql2";
 
 const app = express();
-
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -10,6 +13,11 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 });
+
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 app.get("/", (req, res) => {
   db.query("SELECT 1", (err, result) => {
