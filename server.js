63325d53252f78b6317bdf24e8494db8175f7b65
ThangNgo02 <thang.ngo@tsp.com.vn>
2026-04-17 08:45:59 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mysql from "mysql2";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+app.get("/", (req, res) => {
+  db.query("SELECT 1", (err, result) => {
+    if (err) return res.send("DB error");
+    res.send("API running");
+  });
+});
+
+app.listen(process.env.PORT || 3000);
